84bc6bdb5b17c35410313a9881b7f81c5360e296
enhanjh <enhanjh@gmail.com>
2018-08-26 16:40:46 +0900

    조회쿼리 완성

--- kospi_donchian.py
+++ kospi_donchian.py
@@ -1,43 +1,59 @@
 import mysql.connector as conn
-import dbConfig
+import db_config
 import pandas as pd
 import numpy as np
 
+# installed package
+# mysql connector
+# pandas
+
 # db connection
-cnx = conn.connect(**dbConfig.config)
+cnx = conn.connect(**db_config.config)
 cursor = cnx.cursor()
 
 
 # 0. WHAT TO BUY
 
-date = '20180102'
+date = '20180201'
 item_cd = '122630'
 # 122630 kodex leverage
 # 252670 kodex kospi 200 future inverse 2x
 # 234790 kodex kosdaq 150 leverage
 
-qs_end_avg = (' SELECT A.TRAN_DAY'
-              '      , A.ITEM'
-              '      , LAG(A.END) OVER(PARTITION BY A.ITEM, A.TRAN_DAY ORDER BY A.TRAN_DAY) AS YDAY_END'
-              '      , A.HIGH'
-              '      , A.LOW'
-              '      , A.END'
-              '      , A.AVG_20'
-              '      , A.AVG_120'
-              '   FROM PRICE A'
-              '  WHERE A.TRAN_DAY IN '
-              '                      SELECT POOL_DAY'
-              '                        FROM TRAN_DAY_CAL'
-              '                       WHERE TRAN_DAY = \''.join(date) + '\''
-              '                       UNION ALL '                                                                        
-              '                      SELECT TRAN_DAY'
-              '                        FROM TRAN_DAY_CAL'
-              '                       WHERE TRAN_DAY = \''.join(date) + '\''
-              '    AND A.ITEM = \''.join(item_cd) + '\''
+qs_end_avg = (' select tran_day'
+              '      , item'
+              '      , yday_end'
+              '      , high'
+              '      , low'
+              '      , end'
+              '      , avg_20'
+              '      , avg_120'
+              '   from ('
+              '         select a.tran_day'
+              '              , a.item'
+              '              , lag(a.end) over(partition by a.item order by a.tran_day) as yday_end'
+              '              , a.high'
+              '              , a.low'
+              '              , a.end'
+              '              , a.avg_20'
+              '              , a.avg_120'
+              '           from price a'
+              '          where a.tran_day in ('
+              '                               select pool_day'
+              '                                 from tran_day_cal'
+              '                                where tran_day = \'' + date + '\''
+              '                                union all'
+              '                               select tran_day'
+              '                                 from tran_day_cal'
+              '                                where tran_day = \'' + date + '\''
+              '                              )'
+              '           and a.item = \'' + item_cd + '\''
+              '        ) mst'
+              '  where tran_day = \'' + date + '\''
               )
 
-print qs_end_avg
+print(qs_end_avg)
 
 df_end_avg = pd.read_sql(qs_end_avg, cnx)
 
-df_end_avg.head(5)
+print(df_end_avg.head(5))
