f8ebb89357a19d1c211eb28eaef5db76d2f3b9e5
enhanjh <enhanjh@gmail.com>
2018-08-27 22:45:12 +0900

    api communication module

---
+++ api/api_comm.py
@@ -0,0 +1,37 @@
+# python 3.6 32bit
+# installed package
+# 1. win32com(pywin32)
+
+import win32com.client
+import pythoncom
+
+# instXASession = win32com.client.Dispatch("XA_Session.XASession")
+
+
+class XASessionEventHandler:
+    login_state = 0
+
+    def OnLogin(self, code, msg):
+        if code == "0000":
+            print("로그인 성공")
+            XASessionEventHandler.login_state = 1
+        else:
+            print("로그인 실패")
+
+
+instXASession = win32com.client.DispatchWithEvents("XA_Session.XASession", XASessionEventHandler)
+
+id = "enhanjh"
+passwd = "sens!$33"
+cert_passwd = "lmtls1433))"
+
+instXASession.ConnectServer("hts.ebestsec.co.kr", 20001)
+instXASession.Login(id, passwd, cert_passwd, 0, 0)
+
+while XASessionEventHandler.login_state == 0:
+    pythoncom.PumpWaitingMessages()
+
+num_account = instXASession.GetAccountListCount()
+for i in range(num_account):
+    account = instXASession.GetAccountList(i)
+    print(account)
