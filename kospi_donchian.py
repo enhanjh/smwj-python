b149492047089b6f8e6ba475377f6b0d1880f1ba
enhanjh <enhanjh@gmail.com>
2018-08-28 12:54:12 +0900

    kospi donchian calculating PDC and N

--- kospi_donchian.py
+++ kospi_donchian.py
@@ -2,8 +2,6 @@ import mysql.connector as conn
 import db_config
 import pandas as pd
 import numpy as np
-pd.core.common.is_list_like = pd.api.types.is_list_like
-from pandas_datareader import data, wb
 from datetime import datetime
 
 # installed package
@@ -12,8 +10,8 @@ from datetime import datetime
 # 3. pandas datareader
 
 # db connection
-# cnx = conn.connect(**db_config.config)
-# cursor = cnx.cursor()
+cnx = conn.connect(**db_config.config)
+cursor = cnx.cursor()
 
 
 # 0. WHAT TO BUY
@@ -24,47 +22,30 @@ item_cd = '122630'
 # 252670 kodex kospi 200 future inverse 2x
 # 234790 kodex kosdaq 150 leverage
 
-# qs_end_avg = (' select tran_day'
-#               '      , item'
-#               '      , yday_end'
-#               '      , high'
-#               '      , low'
-#               '      , end'
-#               '      , avg_20'
-#               '      , avg_120'
-#               '   from ('
-#               '         select a.tran_day'
-#               '              , a.item'
-#               '              , lag(a.end) over(partition by a.item order by a.tran_day) as yday_end'
-#               '              , a.high'
-#               '              , a.low'
-#               '              , a.end'
-#               '              , a.avg_20'
-#               '              , a.avg_120'
-#               '           from price a'
-#               '          where a.tran_day in ('
-#               '                               select pool_day'
-#               '                                 from tran_day_cal'
-#               '                                where tran_day = \'' + date + '\''
-#               '                                union all'
-#               '                               select tran_day'
-#               '                                 from tran_day_cal'
-#               '                                where tran_day = \'' + date + '\''
-#               '                              )'
-#               '           and a.item = \'' + item_cd + '\''
-#               '        ) mst'
-#               '  where tran_day = \'' + date + '\''
-#               )
-#
-# print(qs_end_avg)
-#
-# df_end_avg = pd.read_sql(qs_end_avg, cnx)
+qs_end_avg = (' select a.tran_day'
+              '      , a.item'
+              '      , lag(a.end) over(partition by a.item order by a.tran_day) as yday_end'
+              '      , a.high'
+              '      , a.low'
+              '      , a.end'
+#              '      , a.avg_20'
+#              '      , a.avg_120'
+              '   from price a'
+              '  where a.tran_day between date_format(date_sub(\'' + date + '\', interval 1 month),\'%Y%m%d\') and \'' + date + '\''
+              '   and a.item = \'' + item_cd + '\''
+              )
 
-df_end_avg = data.DataReader(
-    "KRX:KOSPI" 
-    ,"google"
-    , datetime(2018, 7, 1)
-    , datetime(2018, 8, 25)
-)
+#print(qs_end_avg)
+
+df_end_avg = pd.read_sql(qs_end_avg, cnx)
+
+df_end_avg["tr1"] = df_end_avg["high"]-df_end_avg["low"]
+df_end_avg["tr2"] = df_end_avg["high"]-df_end_avg["yday_end"]
+df_end_avg["tr3"] = df_end_avg["yday_end"]-df_end_avg["low"]
+df_end_avg["true_range"] = df_end_avg[["tr1","tr2","tr3"]].max(axis=1)
+df_end_avg["N"] = (19 * df_end_avg["yday_end"] + df_end_avg["true_range"]) / 20
 
 print(df_end_avg)
+
+#nda_end_avg = df_end_avg.values
+
